e1009d6108e447a692e1a2050cf4118a7dcb1882
Eric Cornelius <Eric.Cornelius@mandiant.com>
2013-08-20 17:09:57 -0400

    Updated redis tests to clear the queue prior to execution

--- harness.js
+++ harness.js
@@ -6,13 +6,24 @@ var count = 0;
 async.forEachSeries(config.test_cases, function(test, cb) {
   console.log(test.name);
   process.env.test_index = count++;
-  var producer = child_process.spawn('node', ['harness_producer'], {stdio: 'inherit'});
-  var consumer = child_process.spawn('node', ['harness_consumer'], {stdio: 'inherit'});
 
-  producer.on('exit', function() {
-    setTimeout(function() {
-      consumer.kill();
-      cb();
-    }, test.flush_interval);
+  pre_hook(function() {
+    var producer = child_process.spawn('node', ['harness_producer'], {stdio: 'inherit'});
+    var consumer = child_process.spawn('node', ['harness_consumer'], {stdio: 'inherit'});
+
+    producer.on('exit', function() {
+      setTimeout(function() {
+        consumer.kill();
+        cb();
+      }, test.flush_interval);
+    });
   });
 });
+
+function pre_hook(cb) {
+  var redis = require('redis');
+  var client = redis.createClient();
+  client.del('my-queue');
+  client.quit();
+  cb();
+}
